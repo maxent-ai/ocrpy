254f9cf3034f969916f2dc611da128a05b21adbe
bharath g s <royalkingpin@gmail.com>
2022-07-03 17:50:13 +0530

    adds basic logger utils

---
+++ ocrpy/utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+__all__ = ['LOGGER']
+
+LOGGER = logging.getLogger("ocrpy")
+fhandler = logging.FileHandler(filename='ocrpy.log', mode='a')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fhandler.setFormatter(formatter)
+LOGGER.addHandler(fhandler)
+LOGGER.setLevel(logging.DEBUG)
