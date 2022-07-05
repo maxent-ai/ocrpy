1cbf63960f5289a44c7ceadabdb7ba7198974dd7
bharath g s <royalkingpin@gmail.com>
2022-07-05 20:22:53 +0530

    update build for docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,6 @@ release = '0.3.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    #sphinx_rtd_theme
     'sphinx.ext.autodoc',
 ]
 
@@ -48,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo' #sphinx_rtd_theme
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
