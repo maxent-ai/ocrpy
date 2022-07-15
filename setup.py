8dd60edc18d6552ca57754ffa3388bb39d308d34
bharath g s <royalkingpin@gmail.com>
2022-07-15 17:31:19 +0530

    update classifiers

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('ocrpy/__init__.py', 'r') as f:
             version = line.strip().split('=')[1].strip(' \'"')
             break
     else:
-        version = '0.0.1'
+        version = '0.3.6'
 
 with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
@@ -23,7 +23,7 @@ REQUIRES = []
 kwargs = {
     'name': 'ocrpy',
     'version': version,
-    'description': 'unified interface to google vision, aws textract, azure, tesseract OCR, EasyOCR tools.',
+    'description': 'Unified interface to google vision, aws textract, azure and tesseract OCR tools.',
     'long_description': readme,
     'author': 'Maxentlabs',
     'author_email': 'maxentlabsai@gmail.com',
@@ -33,18 +33,35 @@ kwargs = {
     'license': 'MIT/Apache-2.0',
     'classifiers': [
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Financial and Insurance Industry',
+
     ],
     'install_requires': REQUIRES,
     'tests_require': ['coverage', 'pytest'],
     'packages': find_packages(exclude=('tests', 'tests.*')),
-
+    'project_urls': {
+        'Documentation': 'maxentlabs.com/ocrpy/', 
+        'Source': 'https://github.com/maxent-ai/ocrpy'}
 }
 
 #################### BEGIN USER OVERRIDES ####################
